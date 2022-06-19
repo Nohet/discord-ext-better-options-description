97cbdda45d98ac15413dfd97b2da52a3656da746
Nohet <70505658+Nohet@users.noreply.github.com>
2022-06-19 22:59:16 +0200

    Update

--- discord/ext/better_options_description/__init__.py
+++ discord/ext/better_options_description/__init__.py
@@ -6,7 +6,7 @@ __all__ = ["parse_doc"]
 
 def parse_doc(func):
     assert isinstance(func, Command)
-    command_doc = safe_load(func.callback.__doc__)
 
+    command_doc = safe_load(func.callback.__doc__)
     _populate_descriptions(func._params, command_doc)
 
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="discord-ext-better-options-description",
+    version="1.0",
+    description="Discord.py extension to keep your slash commands options neat and tidy.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nohet/discord-ext-better-options-description",
+    author="Nohet",
+    author_email="igorczupryniak503@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=["discord", "discord extension", "discord.py", "slashcommands"],
+    packages=find_packages(),
+    install_requires=["git+https://github.com/Rapptz/discord.py", "PyYAML"],
+)
